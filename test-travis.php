0db007cb5490f13feebfdd1aee3708e3d7d57ccc
Jimmy Huang <jimmy@netivism.com.tw>
2014-10-07 18:27:29 +0800

    Add phpunit test for timezone.

--- test-travis.php
+++ test-travis.php
@@ -1,3 +1,9 @@
 <?php
-$timezone = date_default_timezone_get();
-echo $timezone;
+class TimezoneTest extends PHPUnit_Framework_TestCase
+{
+    public function testTimezone()
+    {
+        $timezone = date_default_timezone_get();
+        $this->assertEquals('Asia/Taipei', $timezone);
+    }
+}
